1dff340d964a90d04e377d1b5fb7846485743f9c
Devin <60894239+NNyaklir@users.noreply.github.com>
2023-06-24 15:16:19 -0600

    updated *token_start

--- src/tokenizer.c
+++ src/tokenizer.c
@@ -41,13 +41,15 @@ char *token_start(char *str){
 
     while (*str != ' '){ //moves the pointer to the end of the word stopping at the whitespace
         *str++;
+
+        if(*str== '\0'){ //in the case of singular word arrays there will be no next word and thus will return 0
+            return 0;
+        }
+        else{
+            return *str; //if there is a next character this should the pointer to it.
+        }
     }
-    if(*str== '\0'){ //in the case of singular word arrays there will be no next word and thus will return 0
-        return 0;
-    }
-    else{
-        return *str; //if there is a next character this should the pointer to it.
-    }
+   
 }
 
 char *token_terminator(char *token){
