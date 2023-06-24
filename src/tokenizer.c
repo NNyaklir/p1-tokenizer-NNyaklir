4f3f6e32a21317e3c41f74ee90eafdcf22fcea85
Devin <60894239+NNyaklir@users.noreply.github.com>
2023-06-24 15:04:54 -0600

    defined *token_start

--- src/tokenizer.c
+++ src/tokenizer.c
@@ -33,3 +33,19 @@ int non_space_char(char c){
         return 1;
     }
 }
+
+char *token_start(char *str){
+    if( str==NULL|| *str=='\0'){ //this checks if the string is empty or the pointer points to nothing
+        return 0;                  // since the str or *str is empty this fufills the return zero condition.
+    }
+
+    while (*str != ' '){ //moves the pointer to the end of the word stopping at the whitespace
+        *str++;
+    }
+    if(*str== '\0'){ //in the case of singular word arrays there will be no next word and thus will return 0
+        return 0;
+    }
+    else{
+        return *str; //if there is a next character this should the pointer to it.
+    }
+}
