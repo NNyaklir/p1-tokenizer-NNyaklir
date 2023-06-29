eba078c407275fd5f5e5ad3e835a9ccc72be100f
Devin <60894239+NNyaklir@users.noreply.github.com>
2023-06-29 11:26:46 -0600

    finished add_history

--- src/history.c
+++ src/history.c
@@ -11,19 +11,33 @@ List* init_history(){
 }
 
 void add_history(List *list, char *str){ //char  *str is a pointer to a string
-    Item *newItem= (Item*)malloc(sizeof(Item));
-    newItem->id=0;
 
+    //this is all the creation of the node itself
+    Item *newItem= (Item*)malloc(sizeof(Item));//creates a node 
+    newItem->id=0;
     int charCount = 0;
     for(int i = 0; str[i] != 0; i++){ //to count the number of characters in the string 
-        int charCheck= non_space_char(str[i]);
-        if(charCheck==0){
-            charCount++;
-        }
+        charCount++;
     }
-    char *data= (char*)malloc(charCount*sizeof(char));
-    *data= copy_str(*str,charCount);
-    newItem->str=data;
-    
+    char *data= (char*)malloc(charCount*sizeof(char));//assignes a memory allocation
+    *data= copy_str(*str,charCount);//copies the strings data for transplant
+    newItem->str=data;//makes the node's str value match the input string
+    newItem->next=NULL;
+    //next we need to check if the linked list has a node in it or if it is empty
 
+    if(list->root == NULL){ //this should make it so that the new node becomes the root
+        list->root=newItem;
+    }
+
+    //next i need to traverse to the end of a linked list to add a new item if the root is not empty
+    if(list->root!=NULL){
+        int traverse=0;
+        Item *current=list->root;//creates a traveresal item
+        while(current->next!=NULL){//goes to the end of the linked list
+            traverse+=1;
+            current=current->next;
+        }
+        newItem->id=traverse;
+        current->next=newItem;//should make the last item of the linked list the newly added item
+    }
 }
