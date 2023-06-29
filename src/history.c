0e8e7767e6825a37b27554232f805e8a9f72ed92
Devin <60894239+NNyaklir@users.noreply.github.com>
2023-06-28 20:16:49 -0600

    Defined init_history

--- src/history.c
+++ src/history.c
@@ -4,4 +4,9 @@
 #include "history.h"
 #include "stdlib.h"
 
+List* init_history(){
+    List *historyList = malloc(sizeof(List)); //in theory it should dynamically allocate memory for a linked list via pointer
+    historyList->root=0; //gives a root to the list
+    return historyList; //returns the newly allocated linked list
+}
 
