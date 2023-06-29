5abf6fa6b3a032451a6f3d5bd66aaed6062831e2
Devin <60894239+NNyaklir@users.noreply.github.com>
2023-06-28 21:15:34 -0600

    partially completed add_history

--- src/history.c
+++ src/history.c
@@ -10,3 +10,20 @@ List* init_history(){
     return historyList; //returns the newly allocated linked list
 }
 
+void add_history(List *list, char *str){ //char  *str is a pointer to a string
+    Item *newItem= (Item*)malloc(sizeof(Item));
+    newItem->id=0;
+
+    int charCount = 0;
+    for(int i = 0; str[i] != 0; i++){ //to count the number of characters in the string 
+        int charCheck= non_space_char(str[i]);
+        if(charCheck==0){
+            charCount++;
+        }
+    }
+    char *data= (char*)malloc(charCount*sizeof(char));
+    *data= copy_str(*str,charCount);
+    newItem->str=data;
+    
+
+}
